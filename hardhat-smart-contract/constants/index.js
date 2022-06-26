8f5e37bd611676b498eaa918fbd811acad14e611
James Adakole <j.adakole@taxaide.com.ng>
2022-06-26 21:09:57 +0100

    smart contract developed and deployed

---
+++ hardhat-smart-contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = '0x69f4831b7A427079d7b1Db6D5a1C007ce7f893A9';
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
